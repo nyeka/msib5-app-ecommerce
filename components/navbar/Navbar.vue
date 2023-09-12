442bcb94b7b3e01deec5e987e89d618e42a6f9ca
nyeka <nyeka2101@gmail.com>
2023-09-12 23:20:57 +0700

    feat: adding route link to sign button

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -72,7 +72,9 @@ const userImage = data?.value?.user?.image;
       <VAvatar :src="(userImage as string)" size="xl" />
     </div>
     <div class="md:flex gap-[24px] items-center hidden" v-else>
-      <button class="text-[#475467]">Log in</button>
+      <button class="text-[#475467]" @click="$router.push('/login')">
+        Log in
+      </button>
       <button
         class="text-[#fff] bg-[#0984DD] flex justify-center rounded-[8px] items-center pl-[18px] pr-[18px] pt-[10px] pb-[10px]"
       >
